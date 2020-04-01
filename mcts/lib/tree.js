556d954a1a64ca16bb265e2f1e38dcc9ca0a9313
vgarciasc <vingarciasc@gmail.com>
2020-04-01 12:13:43 -0300

    adding tree removal operation

--- mcts/lib/tree.js
+++ mcts/lib/tree.js
@@ -15,6 +15,58 @@ class Tree {
 		this.nodes[node.parent_id].children_id.push(node.id);
 	}
 
+	remove(node) {
+		let removed_ids = this.remove_rec(node);
+
+		this.nodes = this.nodes.filter((f) => f);
+
+		for (var i = 0; i < this.nodes.length; i++) {
+			let removed_before_id = this.removed_before(this.nodes[i].id, removed_ids);
+			this.nodes[i].id -= removed_before_id;
+
+			let removed_before_parent_id = this.removed_before(this.nodes[i].parent_id, removed_ids);
+			this.nodes[i].parent_id -= removed_before_parent_id;
+
+			for (var j = 0; j < this.nodes[i].children_id.length; j++) {
+				let removed_before_children_id = this.removed_before(this.nodes[i].children_id[j], removed_ids);
+				this.nodes[i].children_id[j] -= removed_before_children_id;
+			}
+		}
+	}
+
+	remove_rec(node) {
+		let removed = [];
+
+		let children = this.getChildren(node).slice();
+		for (var i = 0; i < children.length; i++) {
+			if (children[i]) {
+				removed = removed.concat(this.remove_rec(children[i]));
+			}
+		}
+
+		let parent_children = this.getParent(node).children_id;
+		let index_of_in_parent = parent_children.indexOf(node.id);
+		
+		if (index_of_in_parent == -1) return;
+
+		this.getParent(node).children_id.splice(index_of_in_parent, 1);
+		this.nodes[node.id] = null;
+
+		removed.push(node.id);
+
+		return removed;
+	}
+
+	removed_before(id, removed_id) {
+		let num = 0;
+		for (var i = 0; i < removed_id.length; i++) {
+			if (removed_id[i] < id) {
+				num += 1;
+			}
+		}
+		return num;
+	}
+
 	update(node, new_data) {
 		this.nodes[node.id].data = new_data;
 	}
@@ -34,6 +86,10 @@ class Tree {
 
 	getSiblings(node) {
 		return this.getChildren(this.getParent(node));
+	}
+
+	getRoot() {
+		return this.get(0);
 	}
 
 	copy() {
